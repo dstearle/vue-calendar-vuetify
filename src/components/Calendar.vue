0f20637f12b8b5c377b12453d032a5ebc3b0fa7f
dstearle <dallas.st.earle@gmail.com>
2020-08-27 13:45:43 -0700

    Created new Calendar component and tested it in main app

---
+++ src/components/Calendar.vue
@@ -0,0 +1,21 @@
+<template>
+
+    <h1>Calendar</h1>
+
+</template>
+
+<script>
+
+    export default {
+
+        components: {
+            
+        },
+
+        data: () => ({
+            //
+        }),
+        
+    };
+
+</script>
